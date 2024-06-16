4b543715c31bc28f3fa325556a07c5f33dc44e4a
A-Veereshwar <151058907+A-Veereshwar@users.noreply.github.com>
2024-06-16 19:55:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//Click to view piano project
+document.getElementById('piano-project').addEventListener('click', function() {
+    window.location.href = 'https://a-veereshwar.github.io/piano-project/';
+});
+//Click to view top10 project
+document.getElementById('top10-website').addEventListener('click', function() {
+    window.location.href = 'https://a-veereshwar.github.io/the-top-10-website/';
+});
+//Click to view QR generater project
+document.getElementById('Qr_Generator').addEventListener('click', function() {
+    window.location.href = 'https://github.com/A-Veereshwar/text-to-QR';
+});
+
